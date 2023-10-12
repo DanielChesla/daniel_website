78137990241fab82b10593cdbadf91aea1940787
danielelectronicpayments <120591889+danielelectronicpayments@users.noreply.github.com>
2023-10-12 13:15:15 -0400

    fixed math

--- math.js
+++ math.js
@@ -1,54 +1,6 @@
 // formulas were taken from various sources on the web, including https://www.cuemath.com/mean-median-mode-formula/ //
 
 
-function max(arr) {
-    let max = arr[0];
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] >= max) {
-            max = arr[i];
-        }
-    }
-    return max;
-}
-
-function min(arr) {
-    let min = arr[0];
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < min) {
-            min = arr[i];
-        }
-    }
-    return min;
-}
-
-function mean(arr) {
-    let sum = 0;
-
-    for (let num of arr) {
-        sum += num;
-    }
-
-    return Math.floor(sum / arr.length);
-}
-
-function median(arr) {
-    arr = arr.sort ((a,b) => a - b);
-
-    if (arr.length %2 !== 0 ) {
-    return arr[Math.floor(arr.length / 2)];
-    }   
-    else {
-        let mid1 = arr[arr.length / 2];
-        let mid2 = arr[arr.length / 2 - 1];
-        return (mid1 + mid2) / 2;
-    }
-}
-
-function range(arr) {
-
-}
 
 function calculate() {
 
@@ -67,3 +19,62 @@ function calculate() {
     document.getElementById("median").innerHTML = "Median: " + median(values);
     document.getElementById("range").innerHTML = "Range: " + (maxVal - minVal);
 }
+
+//finding the largest number
+
+function max(inNum) {
+    let max = inNum[0];
+
+    for (let i = 0; i < inNum.length; i++) {
+        if (inNum[i] >= max) {
+            max = inNum[i];
+        }
+    }
+    return max;
+}
+
+
+//finding the smallest number
+
+function min(inNum) {
+    let min = inNum[0];
+
+    for (let i = 0; i < inNum.length; i++) {
+        if (inNum[i] < min) {
+            min = inNum[i];
+        }
+    }
+    return min;
+}
+
+//finding the mean of the numbers
+
+
+function mean(inNum) {
+    let sum = 0;
+
+    for (let num of inNum) {
+        sum += num;
+    }
+
+    return Math.floor(sum / inNum.length);
+}
+
+
+//finding the median of the numbers
+
+function median(inNum) {
+    arr = inNum.sort ((a,b) => a - b);
+
+    if (inNum.length %2 !== 0 ) {
+    return inNum[Math.floor(inNum.length / 2)];
+    }   
+    else {
+        let mid1 = inNum[inNum.length / 2];
+        let mid2 = inNum[inNum.length / 2 - 1];
+        return (mid1 + mid2) / 2;
+    }
+}
+
+//function range(inNum) {}
+
